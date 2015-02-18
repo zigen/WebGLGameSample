535bdcf6fab2785d9f44730ab66e43b9a2a2a895
hrl7 <hrlclb@gmail.com>
2015-02-18 20:50:00 +0900

    Canvasで四角を描画した

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function main(){
   console.log("Hello world from javascript");
+  draw2dRect();
+}
+
+function draw2dRect(){
+  var cvs = document.getElementById('cvs');
+  var ctx = cvs.getContext('2d');
+  ctx.fillStyle = "red";
+  ctx.fillRect(20,20,50,50);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(40,40,50,50);
 }
