8ee95a768990375c8240374691609c422878dd51
hrl7 <hrlclb@gmail.com>
2015-02-18 21:15:28 +0900

    カメラをCubeの周りでまわってみる

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, WIDTH/HEIGHT, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 
+var cnt  = 0,
+    orbitRadius = 5;
+
 function main(){
   console.log("Hello world from javascript");
   draw3dCube();
@@ -18,19 +21,24 @@ function draw3dCube(){
 
   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
   var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
+
+  cube = new THREE.Mesh( geometry, material );
 
   scene.add( cube );
   camera.position.z = 5;
 
-  renderer.render( scene, camera );
+  render();
 }
 
-function draw2dRect(){
-  var cvs = document.getElementById('cvs');
-  var ctx = cvs.getContext('2d');
-  ctx.fillStyle = "red";
-  ctx.fillRect(20,20,50,50);
-  ctx.fillStyle = "blue";
-  ctx.fillRect(40,40,50,50);
+function update(){
+  camera.position.x = Math.sin(cnt) * orbitRadius;
+  camera.position.z = Math.cos(cnt) * orbitRadius;
+  cnt += 0.01;
 }
+
+function render(){
+update();
+ requestAnimationFrame( render );
+ renderer.render( scene, camera );
+}
+
